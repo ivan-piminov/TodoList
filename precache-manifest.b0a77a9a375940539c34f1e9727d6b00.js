f2ffe0cf3d32745271577dc3efb574706bb4edcc
ivan-piminov <ivan-piminov@yandex.ru>
2020-07-22 19:35:36 +0300

    Updates

---
+++ precache-manifest.b0a77a9a375940539c34f1e9727d6b00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445d1d5495d8543dedcecd3ec284f5b2",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "685ee291fdc0f8e19823",
+    "url": "/TodoList/static/css/main.f3a3d437.chunk.css"
+  },
+  {
+    "revision": "0fde396f14d2b29a4c49",
+    "url": "/TodoList/static/js/2.83c71f1b.chunk.js"
+  },
+  {
+    "revision": "685ee291fdc0f8e19823",
+    "url": "/TodoList/static/js/main.22d725bc.chunk.js"
+  },
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  }
+]);
